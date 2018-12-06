904090530e6f9b2b763b395c6e75292d3ce5f9cb
Xu <503018338@logon.ds.ge.com>
2018-12-06 14:28:45 +0800

    单点登录案例

---
+++ spring-oauth-parent/spring-oauth-server/src/main/java/com/louis/spring/oauth/server/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.louis.spring.oauth.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        	.authorizeRequests()
+        	.antMatchers("/**/*.js").permitAll()
+        	.antMatchers("/**/*.css").permitAll()
+            .antMatchers("/oauth/authorize").permitAll()
+            .antMatchers("/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	// 配置用户名密码，这里采用内存方式，生产环境需要从数据库获取
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password(passwordEncoder().encode("123"))
+            .roles("USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ spring-oauth-parent/spring-oauth-server/src/main/java/com/louis/spring/oauth/server/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.louis.spring.oauth.server.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println(principal);
+        return principal;
+    }
+    
+}
